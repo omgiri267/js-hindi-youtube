c83c841ba478607381b72ed749f3f804c9b96fe7
omgiri267 <168594726+omgiri267@users.noreply.github.com>
2025-04-16 02:42:56 +0000

    ConbersionOpration

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -23,5 +23,37 @@ let booleanIsLoggedIn = Boolean (isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ** Operations **
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "Om"
+let str2 = " Giri"
+let str3 = str1 + str2
+
+//console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+ 
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
