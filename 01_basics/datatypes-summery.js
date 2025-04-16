9d7ef1d14aa39e70893b530945d407c04ebe0247
omgiri267 <168594726+omgiri267@users.noreply.github.com>
2025-04-16 04:02:25 +0000

    datatypes summery

---
+++ 01_basics/datatypes-summery.js
@@ -0,0 +1,35 @@
+// Primitive
+// 7 types: String, Number, Boolean, Nun, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let usreEmail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+//console.log(id === anotherid);
+
+//const bigNumber = 9898989889989898n
+
+
+// Reference (Non Primitive)
+// Array, Objects & Functions
+
+const heros = ["Saktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "omgiri",
+    age: 43,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof anotherid);
+
+
